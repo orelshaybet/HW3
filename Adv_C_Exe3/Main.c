6ec1ca2e55bf2c43366d26fd3ebeb4bb20572f9b
orelshaybet <orelshaybet1@gmail.com>
2022-04-21 00:32:01 +0300

    q2 befor debug

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 
-void print(Stack* s)
+void printstack(Stack* s)
 {
 	charNode* tmp;
 	tmp = s->head;
@@ -16,61 +16,109 @@ void print(Stack* s)
 		tmp = tmp->next;
 	}
 }
+void printline(Queue* line)
+{
+	intNode* tmp;
+	tmp = line->head;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->data);
+		tmp = tmp->next;
+	}
+}
 
 void main()
 {
-	char c;
+	//char c;
 
-	//function 1
-	printf("function number 1: \n");
-	char* arr_sentence1 = "Remem#reb# thi#carp s#tice";
-	flipBetweenHashes(arr_sentence1);
-	printf("\n");
-	char* arr_sentence2 = "#meR#embe#siht r# practice";
-	flipBetweenHashes(arr_sentence2);
+	////function 1.1
+	//printf("function number 1.1: \n");
+	//char* arr_sentence1 = "Remem#reb# thi#carp s#tice";
+	//flipBetweenHashes(arr_sentence1);
+	//printf("\n");
+	//char* arr_sentence2 = "#meR#embe#siht r# practice";
+	//flipBetweenHashes(arr_sentence2);
 
-	//function 2
-	Stack s2;
-	initStack(&s2);
-	int size2 = 0;
-	printf("\n\nfunction number 2: \n");
-	printf("how many chars? ");
-	scanf("%d", &size2);
-	for (int i = 1; i <= size2; i++)
+	////function 1.2
+	//Stack s2;
+	//initStack(&s2);
+	//int size1_2 = 0;
+	//printf("\n\nfunction number 1.2: \n");
+	//printf("how many chars? ");
+	//scanf("%d", &size1_2);
+	//for (int i = 1; i <= size1_2; i++)
+	//{
+	//	printf("enter char num %d:  ", i);
+	//	fseek(stdin, 0, SEEK_END);
+	//	scanf("%c", &c);
+	//	push(&s2, c);
+	//}
+	//int is_palindrome = isPalindrome(&s2);
+	//if (is_palindrome == 1)
+	//	printf("this is a palindrome!");
+	//else
+	//	printf("this is not a palindrome");
+	//
+
+
+	////function 1.3
+	//Stack s3;
+	//initStack(&s3);
+	//int size1_3 = 0;
+	//printf("\n\nfunction number 1.3: \n");
+	//printf("how many chars? ");
+	//scanf("%d", &size1_3);
+	//for (int i = 1; i <= size1_3; i++)
+	//{
+	//	printf("enter char num %d:  ", i);
+	//	fseek(stdin, 0, SEEK_END);
+	//	scanf("%c", &c);
+	//	push(&s3, c);
+	//}
+	//printf("the stack: \n");
+	//printstack(&s3);
+	//int num = 0;
+	//printf("\nhow many chars do you want to move? ");
+	//scanf("%d", &num);
+	//rotateStack(&s3, num);
+	//printstack(&s3);
+
+
+//	//function 2.1
+//	Queue line1;
+//	int n1 = 0;
+//	initQueue(&line1);
+//	int size2_1 = 0;
+//	printf("\n\nfunction number 2.1: \n");
+//	printf("how many numbers? ");
+//	scanf("%d", &size2_1);
+//	for (int i = 1; i <= size2_1; i++)
+//	{
+//		printf("enter number num %d:  ", i);
+//		scanf("%d", &n1);
+//		enqueue(&line1, n1);
+//	}
+//	printf("the line: \n");
+//	printline(&line1);
+//	rotateQueue(&line1);
+//	printf("\nthe line after rotate: \n");
+//	printline(&line1);
+
+
+	//function 2.2
+	Queue line2;
+	int n2 = 0;
+	initQueue(&line2);
+	int size2_2 = 0;
+	printf("\n\nfunction number 2.2: \n");
+	printf("how many numbers? ");
+	scanf("%d", &size2_2);
+	for (int i = 1; i <= size2_2; i++)
 	{
-		printf("enter char num %d:  ", i);
-		fseek(stdin, 0, SEEK_END);
-		scanf("%c", &c);
-		push(&s2, c);
+		printf("enter number num %d:  ", i);
+		scanf("%d", &n2);
+		cutAndReplace(&line2);
 	}
-	int is_palindrome = isPalindrome(&s2);
-	if (is_palindrome == 1)
-		printf("this is a palindrome!");
-	else
-		printf("this is not a palindrome");
-	
-
-
-	//function 3
-	Stack s3;
-	initStack(&s3);
-	int size3 = 0;
-	printf("\n\nfunction number 3: \n");
-	printf("how many chars? ");
-	scanf("%d", &size3);
-	for (int i = 1; i <= size3; i++)
-	{
-		printf("enter char num %d:  ", i);
-		fseek(stdin, 0, SEEK_END);
-		scanf("%c", &c);
-		push(&s3, c);
-	}
-	printf("the stack: \n");
-	print(&s3);
-	int num = 0;
-	printf("\nhow many chars do you want to move? ");
-	scanf("%d", &num);
-	rotateStack(&s3, num);
-	print(&s3);
+	printf("the line: \n");
+	printline(&line2);
 }
-
--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,45 +1,180 @@
 #include "Queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+// my functions //
+int size_of_queue(Queue* q);
+int sum_of_queue(Queue* q);
 
 /***************** Queue ADT Implementation *****************/
 
 void initQueue(Queue* q)
 {
-	// add your code here
+	q->head = NULL;
+	q->tail = NULL;
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	free(q->head);
+	free(q->tail);
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+	intNode* p = (intNode*)malloc(sizeof(intNode));
+	if (p == NULL)
+	{
+		printf("memory allocation problem \n");
+		return;
+	}
+	p->data = data;
+	p->next = NULL;
+	if (isEmptyQueue(q) != 1)
+	{
+		q->tail->next = p;
+	}
+	else
+	{
+		q->head = p;
+	}
+	q->tail = p;
 }
 
 unsigned int dequeue(Queue* q)
 {
-	// add your code here
+	unsigned int temp = q->head->data;
+	intNode* p = q->head;
+	q->head = q->head->next;
+	if (q->head == NULL)
+	{
+		q->tail = NULL;					
+	}
+	free(p);
+	return temp;
+
 }
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+	if (q->head == NULL && q->tail == NULL)
+		return 1;
+	else
+		return 0;
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
 
 void rotateQueue(Queue* q)
 {
-	// add your code here
+	int size = size_of_queue(q);
+	for (int i = 0; i < (size - 1); i++) 
+	{
+		enqueue(q, dequeue(q));				 
+	}
 }
 
 void cutAndReplace(Queue* q)
 {
-	// add your code here
+	Queue helper1;
+	Queue helper2;
+
+	int size = size_of_queue(q);
+	int sum = sum_of_queue(q);
+	if (size % 2 == 1)
+	{
+		enqueue(q, (sum / size));
+	}
+	for (int i = 0; i < (size/2); i++)
+	{
+		enqueue(&helper2, dequeue(q));
+	}
+	for (int i = 0; i < (size / 2); i++)
+	{
+		rotateQueue(&q);
+		enqueue(&helper1, dequeue(q));
+	}
+	for (int i = 0; i < (size / 2); i++)
+	{
+		enqueue(q, dequeue(&helper1));
+	}
+	for (int i = 0; i < (size / 2); i++)
+	{
+		enqueue(q, dequeue(&helper2));
+	}
+	destroyQueue(&helper1);
+	destroyQueue(&helper2);
 }
 
 void sortKidsFirst(Queue* q)
 {
-	// add your code here
+	int size = size_of_queue(q);
+	int* arr = (int*)malloc(size * sizeof(int));
+	if (arr == NULL)
+	{
+		printf("memory allocation problem \n");
+		exit(0);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = dequeue(q);
+	}
+	for (int i = 0; i < size -1; ++i) // arr[i] will be the smallest of i..N-1 
+	{
+		for (int j = i + 1; j < size; ++j)
+		{
+			if (arr[i] > arr[j])
+			{
+				int t = arr[i];
+				arr[i] = arr[j];
+				arr[j] = t;
+			}
+		}
+	}
+	for (int i = 0; i < size; i++)
+	{
+		enqueue(q, arr[i]);
+	}
+	free(arr);
+}
+
+int size_of_queue(Queue* q)
+{
+	Queue tmp;
+	initQueue(&tmp);
+	int counter = 0;
+	while (!isEmptyQueue(q))
+	{
+		unsigned int val = dequeue(q);
+		enqueue(&tmp, val);
+		counter++;
+	}
+	while (!isEmptyQueue(&tmp))
+	{
+		enqueue(q, dequeue(&tmp));
+	}
+	destroyQueue(&tmp);
+	return counter;
+}
+
+int sum_of_queue(Queue* q)
+{
+	Queue tmp;
+	initQueue(&tmp);
+	int sum = 0;
+	while (!isEmptyQueue(q))
+	{
+		unsigned int val = dequeue(q);
+		enqueue(&tmp, val);
+		sum += val;
+	}
+	while (!isEmptyQueue(&tmp))
+	{
+		enqueue(q, dequeue(&tmp));
+	}
+	destroyQueue(&tmp);
+	return sum;
 }
